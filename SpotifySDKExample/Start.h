032367a3e7d0a077bf273397142bc0fad94d3f4c
ferbarragan <ferbarragan.developer@gmail.com>
2016-08-06 12:59:32 -0500

    Initial configuration with Spotify SDK added.

---
+++ SpotifySDKExample/Start.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifySDKExample
+//
+//  Created by Christian Barragan on 06/08/16.
+//  Copyright © 2016 Christian Barragan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Start : UIViewController
+
+
+@end
+
